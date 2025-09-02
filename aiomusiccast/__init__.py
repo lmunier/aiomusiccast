6b1bb1d365a07d6a3e1f35dfbce45d917588b2fe
lmunier <lmunier@protonmail.com>
2025-09-02 21:50:59 +0200

    [Fix] Solve issue on the deprecated import : pkg_ressources.

--- aiomusiccast/__init__.py
+++ aiomusiccast/__init__.py
@@ -1,17 +1,21 @@
-from pkg_resources import DistributionNotFound, get_distribution
+from importlib.metadata import version, PackageNotFoundError
 
 from .musiccast_device import (
     MusicCastDevice,
 )
 from .musiccast_data import MusicCastData, MusicCastZoneData
 
-from .exceptions import MusicCastException, MusicCastConnectionException, MusicCastGroupException
+from .exceptions import (
+    MusicCastException,
+    MusicCastConnectionException,
+    MusicCastGroupException,
+)
 
 from .musiccast_media_content import MusicCastMediaContent
 
 from .features import DeviceFeature, ZoneFeature
 
 try:
-    __version__ = get_distribution('aiomusiccast').version
-except DistributionNotFound:
+    __version__ = version('aiomusiccast')
+except PackageNotFoundError:
     __version__ = '(local)'
